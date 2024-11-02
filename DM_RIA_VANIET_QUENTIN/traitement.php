25ace529c3a1224888a04ee5636eb2ef71afb7d5
realstyle05 <quentinvaniet@orange.fr>
2024-11-02 19:04:15 +0100

    fonction d'exportation de la photo dans les téléchargement

--- DM_RIA_VANIET_QUENTIN/traitement.php
+++ DM_RIA_VANIET_QUENTIN/traitement.php
@@ -1,27 +1,38 @@
 <?php
     include_once "lib_gen.php";
 
-    // importation des tuiles et récupération des variables
-    
-    $largeur = $_GET["x"];
-    $hauteur = $_GET["y"];
+    // récupération de l'action
     $action = $_GET["generate"];
-    $seed = isset($_GET["seed"]) && $_GET["seed"] !== '' ? $_GET["seed"] : null;
-    $tuiles = $_GET["tiles"];
-    // traitement des tuiles
-    $tuiles = "img_Tiles/".$tuiles;
-    $tuiles = imagecreatefrompng($tuiles);
-    $tab_tuiles = sectionnerTuile($tuiles);
 
     // création du labyrinthe
     if($action == "Generer"){
+        // récupération des paramètres
+        $largeur = $_GET["x"];
+        $hauteur = $_GET["y"];
+        $seed = isset($_GET["seed"]) && $_GET["seed"] !== '' ? $_GET["seed"] : null;
+        $tuiles = $_GET["tiles"];
+
+        // récupération des tuiles
+        $tuiles = "img_Tiles/".$tuiles;
+        $tuiles = imagecreatefrompng($tuiles);
+        $tab_tuiles = sectionnerTuile($tuiles);
+
+        // génération du labyrinthe
         $labyrinthe = genererLabyrintheVide($largeur, $hauteur);
         $lab_pf = creerLabyrintheParfait($labyrinthe, $seed);
         genererDepartArrivee($lab_pf['lab']);
         creerImageLabyrinthe($lab_pf['lab'], $tab_tuiles);
 
+        // Redirection vers Labyrinthe.html avec les paramètres nécessaires
+        header("Location: Labyrinthe.html?generate=$action&seed=$lab_pf[seed]");
     }
-    // Redirection vers Labyrinthe.html
-    header("Location: Labyrinthe.html?generate=$action&seed=$lab_pf[seed]");
+    else if($action == "Exporter"){
+        $name = 'imageGenere/labyrinthe.png';
+        header('Content-Type: application/PNG');
+        header('Content-Disposition: attachment; filename="labyrinthe.png"');
+        header('Content-Length: ' . filesize($name));
+        readfile($name);
+        //header('Location : Labyrinthe.html');
+    }
     exit();
 ?>
